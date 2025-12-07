385d4cfdf8cafa06c6cff17e3daa0ce95195f361
tommasopizzonia0 <tommasopizzonia0@gmail.com>
2025-12-07 17:15:02 +0100

    aggiustata tutta la sidebar

--- nutrizionista_front/src/app/components/navbar/navbar.spec.ts
+++ nutrizionista_front/src/app/components/navbar/navbar.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar';
+
+
 
 describe('Navbar', () => {
   let component: NavbarComponent;
